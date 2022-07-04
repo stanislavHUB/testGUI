4056f55312136908300d6c0fb61752e83aaa959a
stanislavHub <budulych.stanislav@gmail.com>
2022-07-04 23:34:37 +0300

    ds

--- src/windows/SecondWindow.java
+++ src/windows/SecondWindow.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SecondWindow extends JFrame{
@@ -14,20 +15,27 @@ public class SecondWindow extends JFrame{
 //    private JTextField inputCow = new JTextField(20);
 //    private JTextField inputDog = new JTextField(20);
     private JLabel labelCat = new JLabel("Cat: ");
-    private JLabel labelCatInfo = new JLabel("Some");
+    private JLabel labelCatInfo = new JLabel("");
 //    private JLabel labelDog = new JLabel("Dog: ");
 //    private JLabel labelCow = new JLabel("Cow: ");
     private JPanel panel =  new JPanel();
     private Cat cat = new Cat();
 
-    public void paint(Graphics g) {
-        g.drawRect(30,100,300,300);
-        g.setColor(Color.GRAY);
-        g.drawRect(355,100,300,300);
-        g.setColor(Color.GRAY);
-        g.drawRect(680,100,300,300);
-        g.setColor(Color.GRAY);
+//    public void paint(Graphics g) {
+//        g.drawRect(30,100,300,300);
+//        g.setColor(Color.GRAY);
+//        g.drawRect(355,100,300,300);
+//        g.setColor(Color.GRAY);
+//        g.drawRect(680,100,300,300);
+//        g.setColor(Color.GRAY);
+//    }
+
+    public void paint(Graphics graph0) {
+        Graphics2D graph = (Graphics2D) graph0;
+        graph.setColor(Color.LIGHT_GRAY);
+        graph.draw(new Rectangle(30,100,300,300));
     }
+
 
     public SecondWindow() {
         setLayout(new BorderLayout(10,10));
@@ -44,19 +52,31 @@ public class SecondWindow extends JFrame{
         panel.add(labelCat);
         panel.add(inputCat);
         panel.add(labelCatInfo);
+        JButton button = new JButton("click");
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pattern pattern1 = Pattern.compile("[0-9]");
+                Pattern pattern2 = Pattern.compile("[a-zA-Z]");
+                Matcher matcher1 = pattern1.matcher(inputCat.getText());
+                Matcher matcher2 = pattern2.matcher(inputCat.getText());
+                if(matcher1.find()){
+                    int number = Integer.parseInt(inputCat.getText());
+                    labelCatInfo.setText(String.valueOf(number+10));
+                }else if(matcher2.find()){
+                    labelCatInfo.setText("I can say " + cat.sound() + " only!");
+                }else if(!matcher1.find() || !matcher2.find()){
+                    labelCatInfo.setText("Error!");
+                }
+            }
+        });
         add(panel, BorderLayout.CENTER);
 
         inputCat.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Pattern pattern1 = Pattern.compile("[0-9]");
-                Pattern pattern2 = Pattern.compile("[a-zA-Z]");
-                if(inputCat.getText().equals(pattern1)){
-                    int number = Integer.parseInt(inputCat.getText());
-                    labelCatInfo.setText(String.valueOf(number+10));
-                }else if(inputCat.getText().equals(pattern2)){
-                    labelCatInfo.setText("I can say " + cat.sound() + " only!");
-                }
+                labelCatInfo.setText(inputCat.getText());
             }
         });
 
